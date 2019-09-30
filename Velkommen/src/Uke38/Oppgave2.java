fb2ca34e72f51a2238fa9a9ccd45e7972f46e427
Matia <Matia@DESKTOP-ESHS9KP>
2019-09-30 12:41:11 +0200

    k

--- Velkommen/src/Uke38/Oppgave2.java
+++ Velkommen/src/Uke38/Oppgave2.java
@@ -1,6 +1,6 @@
 package Uke38;
 
-
+import java.io.*;
 
 import static javax.swing.JOptionPane.*;
 import java.lang.System.*;
@@ -11,72 +11,91 @@ public class Oppgave2 {
 	public static void main(String[] args) {
 	
 		//2a
-		//Ny streng
+		//y streng
+//
+//		String t = showInputDialog("Mønster");
+//		
+//		
+//		int antallord = 5;
+//		String input [];
+//		input = new String[antallord];
+//		for(int i = 0; i <antallord; i++) {
+//		 String s = showInputDialog("Skriv inn String her");
+//		 input[i] = s;
+//		 
+//		}
+//		System.out.print("[");
+//		for(int k = 0; k <input.length; k++) {
+//			System.out.print(input[k]+", ");
+//		}
+//		System.out.println("]");
+//		
+//		
+//	System.out.println("Ditt mønster er " + t +" og er på plass");
+//		//2b
+//	    int plass = 0;
+//	    int antall = 0;
+//		 
+//		for(int k = 0; k < input.length; k++) {
+//			
+//		if( input[k].contentEquals(t)) { 
+//			plass = k;
+//			antall ++;
+//			
+//	       System.out.println((plass+1));
+//	       
+//		}	
+//}
+//		System.out.println("Ditt mønster er " + antall + " ganger i tabellen");
+//		
+////		2c
+//		String k = null;
+//		
+//		for(int i = 0; i < input.length; i++ ) {
+//			k = input[i];
+//			if(k.length() == 1 ){
+//				System.out.println(k);
+//				}
+//			else if (k.length() % 2 == 0) {
+//				System.out.print(k.charAt((k.length()-1)/2));
+//				System.out.println(k.charAt(((k.length()-1)/2)+1));
+//				
+//				}
+//			else if (k.length() >= 3) {
+//				System.out.print(k.charAt((k.length()-2)/2));
+//				System.out.print(k.charAt(((k.length()-2)/2)+1));
+//				System.out.println(k.charAt(((k.length()-2)/2)+2));
+//			}
+//			
+//				
+//			}
 		
-		String t = showInputDialog("Mønster");
+	//
+        String str = "hello";
 		
 		
-		int antallord = 5;
-		String input [];
-		input = new String[antallord];
-		for(int i = 0; i <antallord; i++) {
-		 String s = showInputDialog("Skriv inn String her");
-		 input[i] = s;
-		 
+		byte [] array1 = str.getBytes();
+	
+		int antallchars = 0;
+		int verdi = 0;
+		
+		for(int i = 0; i < array1.length; i++) {	
+			verdi += array1[i];
+			antallchars ++;
 		}
-		System.out.print("[");
-		for(int k = 0; k <input.length; k++) {
-			System.out.print(input[k]+", ");
-		}
-		System.out.println("]");
+	
+		System.out.println(verdi);
+		System.out.println(antallchars);
 		
-		
-//		System.out.println("Ditt mønster er " + t +" og er på plass");
-		//2b
-	    int plass = 0;
-	    int antall = 0;
-		 
-		for(int k = 0; k < input.length; k++) {
-			
-		if( input[k].contentEquals(t)) { 
-			plass = k;
-			antall ++;
-			
-	       System.out.println((plass+1));
-	       
-		}	
-}	
-		
-//		2ck
-		String k = null;
-		
-		for(int i = 0; i < input.length; i++ ) {
-			k = input[i];
-			if(k.length() == 1 ){
-				System.out.println(k);
-				}
-			else if (k.length() % 2 == 0) {
-				System.out.print(k.charAt((k.length()-1)/2));
-				System.out.println(k.charAt(((k.length()-1)/2)+1));
-				
-				}
-			else if (k.length() >= 3) {
-				System.out.print(k.charAt((k.length()-2)/2));
-				System.out.print(k.charAt(((k.length()-2)/2)+1));
-				System.out.println(k.charAt(((k.length()-2)/2)+2));
-			}
-			
-			//2d 
-			
-				
-			}
+		System.out.println(verdi/antallchars);
 		
 	
+		
 		baklengs("test");
 	}
          
 	
-	
+	//2d
 	public static String baklengs(String k) {
 
 		int c = 1;
@@ -93,8 +112,16 @@ public class Oppgave2 {
 		return k;
 	}
 
+	//2e
+		public static String Unikode(String t ) {
+			
+			
+			
+			return t;
+		}
 	
-	}
+}
+	
 //		System.out.println("Mønsteret ditt er " + antall + " ganger i tabellen");
 	//	System.out.print(t.contains("br"));
 	
